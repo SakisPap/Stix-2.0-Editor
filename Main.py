a2d2067c0669b614669d7c0bb7f29e233bd72c4a
Alpha <it14118@uom.edu.gr>
2018-04-14 23:24:34 +0300

    Add GNU comm

--- Main.py
+++ Main.py
@@ -1,3 +1,26 @@
+'''
+This file is part of STIX 2.0 UoM Editor.
+
+STIX 2.0 UoM Editor is free software: you can redistribute it and/or modify
+
+it under the terms of the GNU General Public License as published by
+
+the Free Software Foundation, either version 3 of the License, or
+
+(at your option) any later version.
+
+STIX 2.0 UoM Editor is distributed in the hope that it will be useful,
+
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+
+along with STIX 2.0 UoM Editor.  If not, see <http://www.gnu.org/licenses/>.
+'''
 import tkinter as tk
 from ObjectsPage import Objects
 from PIL import Image, ImageTk

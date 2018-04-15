162badd0c2811584b9340460a7f94132ec519ae7
sakispap <s.papageorgiou@vieconnect.io>
2018-04-15 16:33:23 +0300

    Report Bug frame prettification

--- Main.py
+++ Main.py
@@ -111,7 +111,7 @@ def bugreport_window():
     label = tk.Label(window, text="Please describe the issue with as many details as possible,\nprovide your contact info if you want us to reach you back.")
     label.pack(pady=5)
     reportEntry = tk.Text(window, font=("OpenSans", 9), width=60, height=8, wrap=tk.WORD)
-    reportEntry.pack()
+    reportEntry.pack(padx=10)
 
     submit = tk.Button(window, text="Submit", command = lambda : [(label.config(text="Sending the report...\n", fg="black"), submit.config(text="Please wait..."), label.update(), submit.update(), bugreport(window, reportEntry.get("1.0", tk.END))) if not reportEntry.compare("end-1c", "==", "1.0") else label.config(text="Warning: You cannot submit an empty form!\n", fg="red")])
     submit.pack(pady=10)

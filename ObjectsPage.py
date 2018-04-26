3c99147cbf6cbce0a876f6876bd30e12b7a5cdf7
sakispap <s.papageorgiou@vieconnect.io>
2018-04-26 16:50:30 +0300

    MULTIPAGING and many other things i cannot recall.

--- ObjectsPage.py
+++ ObjectsPage.py
@@ -276,6 +276,7 @@ class Objects(tk.Frame):
 
     def start_Editor(self, type_of_editor):
         if self.object=="nothing" and type_of_editor==1:
+            #set object for when in display all
             self.object=self.full_list[self.listbox.curselection()[0]].split(": ")[0]
             editor = Editor(self, self.masterBody, self.full_list[self.listbox.curselection()[0]].split(": ")[0], type_of_editor)
         else:

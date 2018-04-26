9410966f7364aea5f9dc9122e976a87441f1c953
sakispap <s.papageorgiou@vieconnect.io>
2018-04-26 22:27:24 +0300

    filestoarr2 hotfix

--- RelationshipPage.py
+++ RelationshipPage.py
@@ -78,7 +78,7 @@ class Relationship(tk.Frame):
         # -----------LISTING FOR RELATIONSHIP TAB-----------------------------
 
         for itemname in filestoarr2("alph"):
-            if itemname.get("type") != "relationship":
+            if itemname.get("type") != "relationship" and itemname.get("type") != "marking-definition":
                 self.listboxLeft.insert(tk.END, itemname.get("type") + ": " + itemname.get("name"))
             # stix2obj.get("type") + "-> " + stix2obj.get("name")
 
